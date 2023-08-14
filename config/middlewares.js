2b18b26df947978a2774f4327863f8082f09b650
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2023-08-14 12:11:20 -0400

    subindo

---
+++ config/middlewares.js
@@ -0,0 +1,50 @@
+module.exports = [
+  "strapi::errors",
+  {
+      name: "strapi::security",
+      config: {
+          contentSecurityPolicy: {
+              useDefaults: true,
+              directives: {
+                'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+                  "frame-src": [
+                      "'self'",
+                      "youtube.com",
+                      "www.youtube.com",
+                      "vimeo.com",
+                      "*.vimeo.com",
+                      "facebook.com",
+                      "www.facebook.com",
+                  ],
+                  "connect-src": ["'self'", "https:", "blob:", "*.strapi.io",'data:', 'res.cloudinary.com'],
+                  "img-src": [
+                      "'self'",
+                      "data:",
+                      "blob:",
+                      "dl.airtable.com",
+                      "strapi.io",
+                      "s3.amazonaws.com",
+                      "cdn.jsdelivr.net",
+                  ],
+                  "style-src": ["'self'", "'unsafe-inline'"],
+                  "script-src": [
+                      "'self'",
+                      "cdn.jsdelivr.net",
+                      "blob:",
+                      "https:",
+                  ],
+                  "font-src": ["'self'"],
+                  upgradeInsecureRequests: null,
+              },
+          },
+      },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
